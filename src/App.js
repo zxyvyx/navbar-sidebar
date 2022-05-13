eb1dca38f6c9cec5b4d6feb2118b4fe5782cf663
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2022-05-13 10:20:57 +0700

    Fix: indentation appeareance 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
-	return (
-		<>
-			<Home />
-		</>
-	);
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
